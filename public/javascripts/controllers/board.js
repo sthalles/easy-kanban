75cf71b542b6d227921ab3287b92c62d106837f8
Thalles Silva <thalles753@gmail.com>
2016-11-13 21:05:10 -0200

    added form for creating new tasks

--- public/javascripts/controllers/board.js
+++ public/javascripts/controllers/board.js
@@ -1,4 +1,4 @@
-angular.module('easyKanbanApp').controller('BoardController', ['$scope', 'BoardService', function($scope, BoardService) {
+angular.module('easyKanbanApp').controller('BoardController', ['$scope', 'BoardService', function ($scope, BoardService) {
   $scope.boardColumns = [
     {
       id: 1,
@@ -21,7 +21,7 @@ angular.module('easyKanbanApp').controller('BoardController', ['$scope', 'BoardS
   ];
 
 
-  $scope.selectColumn = function(column, $event) {
+  $scope.selectColumn = function (column, $event) {
     console.log($event.currentTarget);
     for (var i = 0; i < $scope.boardColumns.length; i++) {
       if ($scope.boardColumns[i].isSelected == true) {
@@ -32,63 +32,47 @@ angular.module('easyKanbanApp').controller('BoardController', ['$scope', 'BoardS
     column.isSelected = true;
   };
 
+  $scope.addNewTask = function () {
+    $('#modal1').modal('open');
+  };
+
   $scope.tasks = [
-        {
-       name: 'Tasks 1',
-       description: 'Mockup tasks 1',
-       tag: 'bug',
-       column: 2
-     }, {
-       name: 'Tasks 4',
-       description: 'Mockup tasks 4',
-       tag: 'bug',
-       column: 2
-     }, {
-       name: 'Tasks 5',
-       description: 'Mockup tasks 5',
-       tag: 'bug',
-       column: 2
-     }, {
-       name: 'Tasks 2',
-       description: 'Mockup tasks 2',
-       tag: 'feature',
-       column: 1
-     }, {
-       name: 'Tasks 3',
-       description: 'Mockup tasks 3',
-       tag: 'improvement',
-       column: 3
-     }, {
-       name: 'Tasks 6',
-       description: 'Mockup tasks 6',
-       tag: 'improvement',
-       column: 3
-     }, {
-       name: 'Tasks 6',
-       description: 'Mockup tasks 6',
-       tag: 'improvement',
-       column: 3
-     }, {
-       name: 'Tasks 6',
-       description: 'Mockup tasks 6',
-       tag: 'improvement',
-       column: 3
-     }, {
-       name: 'Tasks 6',
-       description: 'Mockup tasks 6',
-       tag: 'improvement',
-       column: 3
-     }, {
-       name: 'Tasks 6',
-       description: 'Mockup tasks 6',
-       tag: 'improvement',
-       column: 3
-     }, {
-       name: 'Tasks 6',
-       description: 'Mockup tasks 6',
-       tag: 'improvement',
-       column: 3
-     }
+    {
+      name: 'Tasks 1',
+      description: 'Mockup tasks 1',
+      tag: 'bug',
+      creator: 'Thalles',
+      assignedTo: 'Thalles',
+      column: 2
+    }, {
+      name: 'Tasks 4',
+      description: 'Mockup tasks 4',
+      tag: 'bug',
+      creator: 'Thalles',
+      assignedTo: 'Thalles',
+      column: 2
+    }, {
+      name: 'Tasks 5',
+      description: 'Mockup tasks 5',
+      tag: 'bug',
+      creator: 'Thalles',
+      assignedTo: 'Thalles',
+      column: 2
+    }, {
+      name: 'Tasks 2',
+      description: 'Mockup tasks 2',
+      tag: 'feature',
+      creator: 'Thalles',
+      assignedTo: 'Thalles',
+      column: 1
+    }, {
+      name: 'Tasks 3',
+      description: 'Mockup tasks 3',
+      tag: 'improvement',
+      creator: 'Thalles',
+      assignedTo: 'Thalles',
+      column: 3
+    }
   ];
 
 }]);
