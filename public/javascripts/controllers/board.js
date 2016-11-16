cda7be632355adb74ec157481bb75e95a139d64b
Thalles <thalles753@gmail.com>
2016-11-16 07:48:49 -0200

    card appearance improvement

--- public/javascripts/controllers/board.js
+++ public/javascripts/controllers/board.js
@@ -73,40 +73,50 @@
 
     $scope.tasks = [
       {
+        id: '1',
         name: 'Tasks 1',
         description: 'Mockup tasks 1',
         tag: 'bug',
         creator: 'Thalles',
         assignedTo: 'Thalles',
-        column: 2
+        column: 2,
+        points: 5
       }, {
+        id: '2',
         name: 'Tasks 4',
         description: 'Mockup tasks 4',
         tag: 'bug',
         creator: 'Thalles',
         assignedTo: 'Thalles',
-        column: 2
+        column: 2,
+        points: 8
       }, {
+        id: '3',
         name: 'Tasks 5',
         description: 'Mockup tasks 5',
         tag: 'bug',
         creator: 'Thalles',
         assignedTo: 'Thalles',
-        column: 2
+        column: 2,
+        points: 4
       }, {
+        id: '4',
         name: 'Tasks 2',
         description: 'Mockup tasks 2',
         tag: 'feature',
         creator: 'Thalles',
         assignedTo: 'Thalles',
-        column: 1
+        column: 1,
+        points: 3
       }, {
+        id: '4',
         name: 'Tasks 3',
         description: 'Mockup tasks 3',
         tag: 'improvement',
         creator: 'Thalles',
         assignedTo: 'Thalles',
-        column: 3
+        column: 3,
+        points: 10
       }
     ];
 
